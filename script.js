146365c4953654d233033d5bdb7ee4b43dfea6c7
solarcalcados <82035186+solarcalcados@users.noreply.github.com>
2022-09-15 16:51:50 -0300

    smart changes

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
   firebase.initializeApp(firebaseConfig);
   const db = firebase.firestore();
   
-  console.log("mds")
+
 
 
   let nId = 0;
@@ -26,77 +26,44 @@ const cosmeticsExists = document.body.contains(listCosmetics)
 const listdiffuser = document.getElementById("lista-diffuser")
 const diffuserExists = document.body.contains(listCosmetics)
 
-setTimeout(carregamento, 1000*4);
-
-if(oilExists){
-    db.collection("oils").get().then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
-
-            nId++;
-            console.log("Item "+nId)
-
-            let htmlProducts ='';
-            htmlProducts += '<div class="col-6 col-md-3 col-lg-3 p-2 px-md-3">'
-            htmlProducts += '   <div onclick="activeModal('+nId+')" class="item">'
-            htmlProducts += '       <div class="card align-items-center bg-light text-dark" >'
-            htmlProducts += '           <img id="h-image-'+nId+'" class="card-img mx-auto" src="img/'+doc.data().image+'.png" alt="Imagem do card">'
-            htmlProducts += '           <div style="margin-top:-45px" class="title align-bottom pb-2">'
-            htmlProducts += '               <h5 class="text-center" id="h-name-'+nId+'">'+doc.data().name+'</h5>'
-            htmlProducts += '           </div>'
-            htmlProducts += '           <div class=" btnsell product-price shadow py-2 px-4 m-3"><a>Detalhes</a></div>'
-            htmlProducts += '       </div>'
-            htmlProducts += '   </div>'
-            htmlProducts += '   <div class="d-none">'
-            htmlProducts += '       <h2 id="h-subname-'+nId+'">'+doc.data().subname+'</h2>'
-            htmlProducts += '       <p id="h-description-'+nId+'">'+doc.data().description+'</p>'
-            htmlProducts += '       <p id="h-body-'+nId+'">'+doc.data().body+'</p>'
-            htmlProducts += '       <p id="h-mind-'+nId+'">'+doc.data().mind+'</p>'
-            htmlProducts += '       <p id="h-skin-'+nId+'">'+doc.data().skin+'</p>'
-            htmlProducts += '   </div>'
-            htmlProducts += '</div>'
-            document.getElementById("lista-oil").innerHTML+=htmlProducts;
-        });
-    });
-
-} else if(cosmeticsExists){
-    db.collection("cosmetics").get().then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
-
-            nId++;
-            console.log("Item "+nId)
-
-            let htmlProducts ='';
-            htmlProducts += '<div class="col-6 col-md-3 col-lg-3 p-2 px-md-3">'
-            htmlProducts += '   <div onclick="activeModal('+nId+')" class="item">'
-            htmlProducts += '       <div class="card align-items-center bg-light text-dark" >'
-            htmlProducts += '           <img id="h-image-'+nId+'" class="card-img mx-auto" src="img/'+doc.data().image+'.png" alt="Imagem do card">'
-            htmlProducts += '           <div style="margin-top:-45px" class="title align-bottom pb-2">'
-            htmlProducts += '               <h5 class="text-center" id="h-name-'+nId+'">'+doc.data().name+'</h5>'
-            htmlProducts += '           </div>'
-            htmlProducts += '           <div class=" btnsell product-price shadow py-2 px-4 m-3"><a>Detalhes</a></div>'
-            htmlProducts += '       </div>'
-            htmlProducts += '   </div>'
-            htmlProducts += '   <div class="d-none">'
-            htmlProducts += '       <h2 id="h-subname-'+nId+'">'+doc.data().subname+'</h2>'
-            htmlProducts += '       <p id="h-description-'+nId+'">'+doc.data().description+'</p>'
-            htmlProducts += '       <p id="h-body-'+nId+'">'+doc.data().body+'</p>'
-            htmlProducts += '       <p id="h-mind-'+nId+'">'+doc.data().mind+'</p>'
-            htmlProducts += '       <p id="h-skin-'+nId+'">'+doc.data().skin+'</p>'
-            htmlProducts += '   </div>'
-            htmlProducts += '</div>'
-            document.getElementById("lista-cosmetics").innerHTML+=htmlProducts;
-        });
-    });
-}
+const listEdit = document.getElementById("lista-edit")
+const isEditPage = document.body.contains(listEdit)
 
 var modal = document.querySelector(".product-modal")
 var bgm = document.querySelector(".bg-modal")
 
-function activeModal(num){
+
+showLoadingScreenFor(4000)
+
+
+
+
+
+
+if(oilExists){carregarOleos("lista-oil");
+} else if(cosmeticsExists){ carregarCosmeticos("lista-cosmetics"); }
+
+console.log(isEditPage)
+if(isEditPage){
+    carregarOleos("lista-edit");
+    carregarCosmeticos("lista-edit");
+}
+
+
+////////////////////////// FUNÇÕES ////////////////////////////
+
+function toggleModalOfDetails(num){
+    loadDataInDetailsModal(num);
+    modal.classList.toggle("active");
+    bgm.classList.toggle("bg-modal-atv");  
+}
+
+function loadDataInDetailsModal(num){
+
+    //Verifica se o modal já está aberto; Se sim, ele não carrega nada
+    if(!(document.getElementById("h-name-"+num))){
+        return
+    }
 
     let name = document.getElementById("h-name-"+num).innerText;
     let image = document.getElementById("h-image-"+num).src.toString();
@@ -105,8 +72,8 @@ function activeModal(num){
     let body = document.getElementById("h-body-"+num).innerText
     let mind = document.getElementById("h-mind-"+num).innerText
     let skin = document.getElementById("h-skin-"+num).innerText
-    modal.classList.toggle("active")
-    bgm.classList.toggle("bg-modal-atv")
+
+
     document.getElementById("product-name").innerHTML=name
     document.getElementById("product-subname").innerHTML='<i>'+subname+'</i>'
     document.getElementById("product-name2").innerHTML=name
@@ -122,18 +89,15 @@ function activeModal(num){
     document.getElementById("product-skin").innerHTML=skin
 }
 
-function desactiveModal(){
-    modal.classList.toggle("active")
-    
-    bgm.classList.toggle("bg-modal-atv")
+function showLoadingScreenFor(timeInMili){
+    setTimeout(toggleLoadingScreen, timeInMili);
 }
 
-function carregamento(){
+function toggleLoadingScreen(){
     document.getElementById("loading").classList.add("none");
-    console.log("oi")
 }
 
-function cadastrarParceiro() {
+function cadastrarProduto() {
     let inputName = document.querySelector("#GET-name").value;
 
     let inputSubname = document.querySelector("#GET-subname").value;
@@ -162,5 +126,71 @@ function cadastrarParceiro() {
     })
     .catch((error) => {
         console.error("Error writing document: ", error);
+    });
+}
+
+function carregarCosmeticos(list){
+    db.collection("cosmetics").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+
+            nId++;
+            console.log("Item "+nId)
+
+            let htmlProducts ='';
+            htmlProducts += '<div class="col-6 col-md-3 col-lg-3 p-2 px-md-3">'
+            htmlProducts += '   <div onclick="toggleModalOfDetails('+nId+')" class="item">'
+            htmlProducts += '       <div class="card align-items-center bg-light text-dark" >'
+            htmlProducts += '           <img id="h-image-'+nId+'" class="card-img mx-auto" src="img/'+doc.data().image+'.png" alt="Imagem do card">'
+            htmlProducts += '           <div style="margin-top:-45px" class="title align-bottom pb-2">'
+            htmlProducts += '               <h5 class="text-center" id="h-name-'+nId+'">'+doc.data().name+'</h5>'
+            htmlProducts += '           </div>'
+            htmlProducts += '           <div class="btnsell product-price shadow py-2 px-4 m-3"><a>Detalhes</a></div>'
+            htmlProducts += '       </div>'
+            htmlProducts += '   </div>'
+            htmlProducts += '   <div class="d-none">'
+            htmlProducts += '       <h2 id="h-subname-'+nId+'">'+doc.data().subname+'</h2>'
+            htmlProducts += '       <p id="h-description-'+nId+'">'+doc.data().description+'</p>'
+            htmlProducts += '       <p id="h-body-'+nId+'">'+doc.data().body+'</p>'
+            htmlProducts += '       <p id="h-mind-'+nId+'">'+doc.data().mind+'</p>'
+            htmlProducts += '       <p id="h-skin-'+nId+'">'+doc.data().skin+'</p>'
+            htmlProducts += '   </div>'
+            htmlProducts += '</div>'
+            document.getElementById(list).innerHTML+=htmlProducts;
+        });
+    });
+}
+
+function carregarOleos(list){
+    db.collection("oils").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+
+            nId++;
+            console.log("Item "+nId)
+
+            let htmlProducts ='';
+            htmlProducts += '<div class="col-6 col-md-3 col-lg-3 p-2 px-md-3">'
+            htmlProducts += '   <div onclick="toggleModalOfDetails('+nId+')" class="item">'
+            htmlProducts += '       <div class="card align-items-center bg-light text-dark" >'
+            htmlProducts += '           <img id="h-image-'+nId+'" class="card-img mx-auto" src="img/'+doc.data().image+'.png" alt="Imagem do card">'
+            htmlProducts += '           <div style="margin-top:-45px" class="title align-bottom pb-2">'
+            htmlProducts += '               <h5 class="text-center" id="h-name-'+nId+'">'+doc.data().name+'</h5>'
+            htmlProducts += '           </div>'
+            htmlProducts += '           <div class=" btnsell product-price shadow py-2 px-4 m-3"><a>Detalhes</a></div>'
+            htmlProducts += '       </div>'
+            htmlProducts += '   </div>'
+            htmlProducts += '   <div class="d-none">'
+            htmlProducts += '       <h2 id="h-subname-'+nId+'">'+doc.data().subname+'</h2>'
+            htmlProducts += '       <p id="h-description-'+nId+'">'+doc.data().description+'</p>'
+            htmlProducts += '       <p id="h-body-'+nId+'">'+doc.data().body+'</p>'
+            htmlProducts += '       <p id="h-mind-'+nId+'">'+doc.data().mind+'</p>'
+            htmlProducts += '       <p id="h-skin-'+nId+'">'+doc.data().skin+'</p>'
+            htmlProducts += '   </div>'
+            htmlProducts += '</div>'
+            document.getElementById(list).innerHTML+=htmlProducts;
+        });
     });
 }
